0b740107197092977362058f5432292f038a02f2
Kirito Msao <55633676+abdelilahelhaddad@users.noreply.github.com>
2023-11-10 18:46:11 +0100

    :iphone: :seedling: Mobile Responsive :sparkles: :iphone:

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -5,3 +5,27 @@ mobileMenu.addEventListener("click", () => {
   body.classList.toggle("opened-menu");
   mobileMenu.classList.toggle("show");
 });
+
+$(".owl-carousel").owlCarousel({
+  center: true,
+  nav: false,
+  lazyLoad: true,
+  loop: true,
+  margin: 10,
+  responsiveClass: true,
+  autoplay: true,
+  autoplayTimeout: 2000,
+  responsive: {
+    0: {
+      items: 1,
+      dots: true,
+      dotsEach: true,
+    },
+    769: {
+      items: 2,
+    },
+    1024: {
+      items: 3,
+    },
+  },
+});
